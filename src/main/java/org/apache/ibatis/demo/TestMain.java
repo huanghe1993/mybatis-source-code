4476c02599cd948f6a1d11b754d4c22d196852e3
huanghe <735597346@qq.com>
2022-03-30 22:09:28 +0800

    mybatis-init

---
+++ src/main/java/org/apache/ibatis/demo/TestMain.java
@@ -0,0 +1,27 @@
+package org.apache.ibatis.demo;
+
+import org.apache.ibatis.demo.entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TestMain {
+    public static void main(String[] args) throws IOException {
+        // 指定全局配置文件
+        String resource = "mybatis-config.xml";
+        // 读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 构建sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        // 获取sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 操作CRUD，第一个参数：指定statement，规则：命名空间+“.”+statementId
+        // 第二个参数：指定传入sql的参数：这里是用户id
+        User user = sqlSession.selectOne("DemoMapper.selectUser", 1);
+        System.out.println(user.getName());
+    }
+}
---
+++ src/main/java/org/apache/ibatis/demo/entity/User.java
@@ -0,0 +1,12 @@
+package org.apache.ibatis.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String id;
+
+    private String name;
+
+}
